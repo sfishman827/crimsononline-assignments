6d54b54a842759d8eb4b1fd17af4c08397909e70
Sam Fishman <sfishman@college.harvard.edu>
2013-03-19 23:42:22 -0400

    finished part0 of midterm

--- midpoint/part0/mysite/polls/urls.py
+++ midpoint/part0/mysite/polls/urls.py
@@ -1,8 +1,30 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import DetailView, ListView
+from polls.models import Poll
+
+#SLOW METHOD:
+#urlpatterns = patterns('polls.views',
+#	url(r'^$', 'index'),
+#	url(r'^(?P<poll_id>\d+)/$', 'detail'),
+#	url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+#	url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
+#)
 
 urlpatterns = patterns('polls.views',
-	url(r'^$', 'index'),
-	url(r'^(?P<poll_id>\d+)/$', 'detail'),
-	url(r'^(?P<poll_id>\d+)/results/$', 'results'),
+	url(r'^$', 
+		ListView.as_view(
+			queryset = Poll.objects.order_by('-pub_date')[:5],
+			context_object_name = 'latest_polls',
+			template_name = 'polls/index.html')),
+	url(r'^(?P<pk>\d+)/$',
+		DetailView.as_view(
+			model = Poll,
+			template_name = 'polls/detail.html'),
+		name='poll_detail'),
+	url(r'^(?P<pk>\d+)/results/$',
+		DetailView.as_view(
+			model = Poll,
+			template_name = 'polls/results.html'),
+		name='poll_results'),
 	url(r'^(?P<poll_id>\d+)/vote/$', 'vote'),
 )
--- midpoint/part0/mysite/polls/views.py
+++ midpoint/part0/mysite/polls/views.py
@@ -1,8 +1,9 @@
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from polls.models import Poll, Choice
-from django.template import Context, loader
+from django.template import Context, loader, RequestContext
 from django.shortcuts import render_to_response, get_object_or_404
-
+from django.core.urlresolvers import reverse
+'''
 def index(request):
 	latest = Poll.objects.all().order_by('-pub_date')[:5]
 	t = loader.get_template('polls/index.html')
@@ -20,11 +21,24 @@ def detail(request, poll_id):
 	#SHORTCUT:
 	p = get_object_or_404(Poll, pk=poll_id)	
 	
-	return render_to_response('polls/detail.html', {'poll': p})
+	return render_to_response('polls/detail.html', {'poll': p}, context_instance=RequestContext(request))
 	#return HttpResponse("This is poll %s." % poll_id)
 	
 def results(request, poll_id):
-	return HttpResponse("This is poll results %s." % poll_id)
-	
+	p = get_object_or_404(Poll, pk=poll_id)
+	return render_to_response('polls/results.html', {'poll': p})
+	#return HttpResponse("This is poll results %s." % poll_id)
+'''	
 def vote(request, poll_id):
+	p = get_object_or_404(Poll, pk=poll_id)
+	try:
+		selection = p.choice_set.get(pk=request.POST['choice'])
+	except (KeyError, Choice.DoesNotExist):
+		return render_to_response('polls/detail.html', {'poll':p, 'error_message': "You didn't select anything"},
+			context_instance=RequestContext(request))
+	else:
+		selection.votes += 1
+		selection.save()
+		return HttpResponseRedirect(reverse('poll_results', args=(p.id,)))
+		#return HttpResponseRedirect(reverse('polls.views.results', args=(p.id,)))
 	return HttpResponse("This is poll vote %s." % poll_id)
